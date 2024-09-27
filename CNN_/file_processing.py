7fdbcd19cbda8cfc90c24280c28f4d0e60200d66
ara.esf <60077649+araesf@users.noreply.github.com>
2024-09-27 15:57:27 -0700

    Update image classification method

--- CNN_/file_processing.py
+++ CNN_/file_processing.py
@@ -1,11 +1,26 @@
 import os
-import nibabel as nib
+from PIL import image
 import numpy as np
 
 # root directory of tumor dataset
 root_dir = r'C:\Users\Ara\Desktop\brain_tumor_dataset'
 
 for image_class in os.listdir(data_dr):
+    # path to categorized images
+    class_dir = os.path.join(root_dir, image_class)
+
+    if os.path.isdir(class_dir):
+        if image_class == "yes":
+            for images in os.listdir(image_class):
+                yes_image = os.path.join(class_dir, images)
+                image_processing = Image.open(yes_image)
+                yes
+                
+            
+            
+            
+
+    
     
 
 
