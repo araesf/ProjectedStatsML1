fc6447254dc7fe149eeeab34587c4ae5873ae46a
ara.esf <ara.esfarjani@gmail.com>
2024-09-26 19:10:20 -0700

    start the project

--- CNN_/convolutional_layers.py
+++ CNN_/convolutional_layers.py
@@ -5,3 +5,4 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 import time
 import os
+
---
+++ CNN_/file_processing.py
@@ -0,0 +1,11 @@
+import os
+import nibabel as nib
+import numpy as np
+
+# root directory of tumor dataset
+root_dir = r'C:\Users\Ara\Desktop\brain_tumor_dataset'
+
+for image_class in os.listdir(data_dr):
+    
+
+
