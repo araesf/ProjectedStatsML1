8d627a914c89fe3ecd1c77cb0f2bc03289f27d55
ara.esf <ara.esfarjani@gmail.com>
2024-09-29 14:07:35 -0700

    incerase sample size, less risk of overfitting

--- data_processing.py
+++ data_processing.py
@@ -4,36 +4,68 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
+root_dir = r'C:\Users\Ara\Desktop\archive\Testing'
+
 class TumorDataProcessor:
     def __init__(self, root_dir, labels, img_size=200):
         self.root_dir = root_dir
         self.labels = labels
         self.img_size = img_size
         self.training_data = []
+        self.validation_data = []
+
+class TumorDataProcessor:
+    def __init__(self, root_dir, labels, img_size=200):
+        self.root_dir = root_dir
+        self.labels = labels
+        self.img_size = img_size
+        self.training_data = []
+        self.validation_data = []
 
     def create_training_data(self):
+        training_dir = os.path.join(self.root_dir, "Training")
         for label in self.labels:
-            class_dir = os.path.join(self.root_dir, label)
-            for brain_scan in os.listdir(class_dir):
-                img_path = os.path.join(class_dir, brain_scan)
-                cv_image = cv2.imread(img_path)
-                if cv_image is not None:
-                    modified_cv_image = cv2.resize(cv_image, (self.img_size, self.img_size))
-                    self.training_data.append([modified_cv_image, label])
-                    print(f'Added scan to training list: {brain_scan}')
+            class_dir = os.path.join(training_dir, label)
+            if os.path.isdir(class_dir):
+                for brain_scan in os.listdir(class_dir):
+                    img_path = os.path.join(class_dir, brain_scan)
+                    cv_image = cv2.imread(img_path)
+                    if cv_image is not None:
+                        modified_cv_image = cv2.resize(cv_image, (self.img_size, self.img_size))
+                        self.training_data.append([modified_cv_image, label])
+                        print(f'Added scan to training list: {brain_scan}')
+                    else:
+                        print(f'Failed to load image: {brain_scan}')
 
-    def get_training_data(self):
-        return self.training_data
+    def create_validation_data(self):
+        testing_dir = os.path.join(self.root_dir, "Testing")
+        for label in self.labels:
+            class_dir = os.path.join(testing_dir, label)
+            if os.path.isdir(class_dir):
+                for brain_scan in os.listdir(class_dir):
+                    img_path = os.path.join(class_dir, brain_scan)
+                    cv_image = cv2.imread(img_path)
+                    if cv_image is not None:
+                        modified_cv_image = cv2.resize(cv_image, (self.img_size, self.img_size))
+                        self.validation_data.append([modified_cv_image, label])
+                        print(f'Added scan to validation list: {brain_scan}')
+                    else:
+                        print(f'Failed to load image: {brain_scan}')
 
 
 def convert_to_tensor(data):
     X = []
     y = []
     for img, label in data:
-        img = np.transpose(img, (2, 0, 1))  # HWC to CHW
-        img = img / 255.0  # Normalize pixel values
+        img = np.transpose(img, (2, 0, 1))
+        img = img / 255.0
         X.append(img)
-        y.append(1 if label == "withTumor" else 0)
+
+        # make model more specific later in model analysis.
+        if label == "glioma" or label == "meningioma" or label == "pituitary":
+            y.append(0)
+        if label == "notumor":
+            y.append(1)
     
     X_array = np.array(X, dtype=np.float32)
     y_array = np.array(y, dtype=np.float32)
@@ -49,7 +81,6 @@ def load_data(root_dir, labels):
     processor.create_training_data()
     training_data = processor.get_training_data()
     
-    train_size = int(0.8 * len(training_data))
     val_size = len(training_data) - train_size
     train_data, val_data = torch.utils.data.random_split(training_data, [train_size, val_size])
 
